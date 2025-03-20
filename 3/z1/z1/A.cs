9fa3a40edddc16b1e3df473d74e609a11ae07032
TanyaDombrovskaya <dombrovskaya006@mail.ru>
2025-03-20 08:54:22 +0300

    3_1

---
+++ 3/z1/z1/A.cs
@@ -0,0 +1,25 @@
+﻿namespace ANamespace
+{
+    class A
+    {
+        private int _a;
+        private int _b;
+
+        public A(int a, int b)
+        { 
+            _a = a;
+            _b = b;
+        }
+
+        public double Func1()
+        {
+            return (Math.Sin(_b) + 4) / 2 * _a;
+        }
+
+        public double Func2()
+        {
+            return Math.Pow(_a + _b, 2);
+        }
+
+    }
+}
---
+++ 3/z1/z1/Program.cs
@@ -0,0 +1,20 @@
+﻿using ANamespace;
+
+class Task1
+{
+    static void Main()
+    {
+        Console.Write("Введите a: ");
+        int a = Convert.ToInt32 (Console.ReadLine());
+
+        Console.Write("Введите b: ");
+        int b = Convert.ToInt32 (Console.ReadLine());
+
+        A c = new(a, b);
+        double result1 = c.Func1();
+        double result2 = c.Func2();
+
+        Console.WriteLine(result1);
+        Console.WriteLine(result2);
+    }
+}
