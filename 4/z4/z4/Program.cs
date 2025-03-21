56130102f6639551f5bee06f9f0e7fa5dadb2c38
TanyaDombrovskaya <dombrovskaya006@mail.ru>
2025-03-21 11:36:55 +0300

    4_4

---
+++ 4/z4/z4/Program.cs
@@ -0,0 +1,30 @@
+﻿class Task4
+{
+    static void Main()
+    {
+        Console.Write("Введите строку: ");
+        string line = Console.ReadLine();
+
+        string result = FirstLetters(line);
+        Console.WriteLine($"Первые буквы слов: {result}");
+    }
+
+    static string FirstLetters(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+        return string.Empty;
+        }
+
+        string result = "";
+
+        string[] words = input.Split(' ');
+
+        foreach (string word in words)
+        {
+            result += word[0];
+        }
+
+        return result;
+    }
+}
