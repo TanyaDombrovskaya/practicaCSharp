e104d12eb56d2a259154046ca226a671545d8a6d
TanyaDombrovskaya <dombrovskaya006@mail.ru>
2025-03-21 11:26:03 +0300

    4_3

---
+++ 4/z3/z3/Program.cs
@@ -0,0 +1,39 @@
+﻿class Task3
+{
+    static void Main()
+    {
+        Console.Write("Введите количество элементов: ");
+        int count = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[count];
+
+        Random random = new();
+
+        for (int i = 0; i < count; i++)
+        {
+            array[i] = random.Next(1, 25);
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+
+        int min = FindMin(array, 0 , array.Length - 1);
+        Console.WriteLine($"\nМинимальный элемент: {min}");
+    }
+
+    static int FindMin(int[] array, int start, int end)
+    {
+        if (start == end)
+        {
+            return array[start];
+        }
+        int mid = (start + end) / 2;
+
+        int leftMin = FindMin(array, start, mid);
+        int rightMin = FindMin(array, mid + 1, end);
+
+        return Math.Min(leftMin, rightMin);
+    }
+}
