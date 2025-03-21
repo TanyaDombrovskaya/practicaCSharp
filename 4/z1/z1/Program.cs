de38b8cdd447e57daeb68815e3c1edd20e94f5b9
TanyaDombrovskaya <dombrovskaya006@mail.ru>
2025-03-21 10:38:58 +0300

    4_1

---
+++ 4/z1/z1/Program.cs
@@ -0,0 +1,20 @@
+﻿class Task1
+{
+    static void Main()
+    {
+        Console.Write("Введите строку: ");
+        string line = Console.ReadLine();
+
+        int count = CountOfWords(line);
+        Console.WriteLine($"Количество слов в строке: {count}");
+    }
+
+    static int CountOfWords(string line)
+    { 
+        string[] words = line.Split(' ');
+
+        int count = words.Length;
+
+        return count;
+    }
+}
