8e644f3531b83fe361631d11bbc12a7068aff0aa
TanyaDombrovskaya <dombrovskaya006@mail.ru>
2025-03-21 11:52:39 +0300

    4_5

---
+++ 4/z5/z5/Food.cs
@@ -0,0 +1,38 @@
+﻿namespace CookNamespace
+{
+    abstract class Food
+    {
+        public abstract void Cook();
+
+        public virtual void Serve() 
+        {
+            Console.WriteLine("The dish is served");
+        }
+    }
+
+    class Pizza : Food
+    {
+        public override void Cook()
+        {
+            Console.WriteLine("Pizza is cooking.");
+        }
+
+        public override void Serve()
+        {
+            Console.WriteLine("The pizza is served.");
+        }
+    }
+
+    class Pasta : Food
+    {
+        public override void Cook()
+        {
+            Console.WriteLine("Pasta is cooking.");
+        }
+
+        public override void Serve()
+        {
+            Console.WriteLine("The pasta is served.");
+        }
+    }
+}
---
+++ 4/z5/z5/Program.cs
@@ -0,0 +1,16 @@
+﻿using CookNamespace;
+
+class Task5
+{
+    static void Main()
+    {
+        Food pizza = new Pizza();
+        Food pasta = new Pasta();
+
+        pizza.Cook();
+        pasta.Cook();
+
+        pizza.Serve();
+        pasta.Serve();
+    }
+}
