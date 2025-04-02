35df89423dc6368aaec784c55b29ec535796db24
TanyaDombrovskaya <dombrovskaya006@mail.ru>
2025-04-02 10:40:16 +0300

    14

---
+++ 12-13-14/Cinoteatr/Cinoteatr/MovieSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cinoteatr
+{
+    public class MovieSession
+    {
+        public string MovieTitle { get; set; }
+        public DateTime SessionTime { get; set; }
+
+        public MovieSession(string title, DateTime time)
+        {
+            MovieTitle = title;
+            SessionTime = time;
+        }
+    }
+}
