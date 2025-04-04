0a5bde3890a7e6278e7b5ce69194e9c0537bcdee
TanyaDombrovskaya <dombrovskaya006@mail.ru>
2025-04-04 14:55:53 +0300

    16

---
+++ 12-13-14-15-16/Cinoteatr/Cinoteatr/PipeClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinoteatr
+{
+    class PipeClient
+    {
+        static void Main(string[] args)
+        {
+            using (var pipeClient = new NamedPipeClientStream(".", "ChatPipe", PipeDirection.InOut))
+            {
+                try
+                {
+                    pipeClient.Connect();
+                    Console.WriteLine("Подключено к серверу.");
+
+                    using (var reader = new StreamReader(pipeClient))
+                    using (var writer = new StreamWriter(pipeClient) { AutoFlush = true })
+                    {
+                        while (true)
+                        {
+                            Console.Write("Введите сообщение: ");
+                            string message = Console.ReadLine();
+                            if (string.IsNullOrWhiteSpace(message)) break;
+
+                            writer.WriteLine(message);
+                            Console.WriteLine("Сообщение отправлено.");
+
+                            string response = reader.ReadLine();
+                            Console.WriteLine("Ответ от сервера: " + response);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Произошла ошибка: " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ 12-13-14-15-16/Cinoteatr/Cinoteatr/login.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows;
+using Newtonsoft.Json;
+using System.IO.MemoryMappedFiles;
+
+namespace Cinoteatr
+{
+    /// <summary>
+    /// Логика взаимодействия для login.xaml
+    /// </summary>
+    public partial class login : Window
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string email = emailTextBox.Text;
+            string password = passwordBox.Password;
+
+            UsersList usersList = LoadUsersFromJson("users.json");
+
+            User loggedInUser = usersList.Users.FirstOrDefault(user => user.Email == email && user.Password == password);
+
+            if (loggedInUser != null)
+            {
+                this.Hide();
+                MainWindow main = new MainWindow();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Неверные учетные данные. Попробуйте снова.");
+            }
+
+            using (var mmf = MemoryMappedFile.CreateNew("CinemaSessions", 1024))
+            {
+                using (var stream = mmf.CreateViewStream())
+                {
+                    using (var writer = new StreamWriter(stream))
+                    {
+                        writer.AutoFlush = true;
+
+                        writer.WriteLine("Новый сеанс: Фильм 'SuperMan' в 19:00 на 01.10.2023");
+                    }
+                }
+            }
+        }
+
+        private UsersList LoadUsersFromJson(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string json = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<UsersList>(json);
+            }
+        }
+    }
+}
