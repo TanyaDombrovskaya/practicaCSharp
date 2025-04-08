2dad1a313961cdee7f17f6b2aa0eebd0853531cc
TanyaDombrovskaya <dombrovskaya006@mail.ru>
2025-04-08 16:17:51 +0300

    19

---
+++ 19/RecipeBook/RecipeBook/Controllers/RecipeStorage.cs
@@ -0,0 +1,14 @@
+﻿using RecipeBook.Models;
+
+namespace RecipeBook.Controllers
+{
+    public static class RecipeStorage
+    {
+        public static List<Recipe> Recipes { get; } = new List<Recipe>
+        {
+            new Recipe { Id = 1, Title = "Паста", Ingredients = "Паста, Вода, Соль", Steps = "Варить пасту в воде." },
+            new Recipe { Id = 2, Title = "Салат", Ingredients = "Огурцы, Помидоры, Оливковое масло", Steps = "Смешать все ингредиенты." },
+            new Recipe { Id = 3, Title = "Омлет", Ingredients = "Яйца, Молоко, Соль", Steps = "Взбить яйца и пожарить." }
+        };
+    }
+}
---
+++ 19/RecipeBook/RecipeBook/Models/Recipe.cs
@@ -0,0 +1,10 @@
+﻿namespace RecipeBook.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Ingredients { get; set; } 
+        public string Steps { get; set; }
+    }
+}   
